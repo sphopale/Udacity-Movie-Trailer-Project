5f15c4bd4ca26876f9f230e927d38d6bd83f103d
Sphurti <sphurti.phopale@gmail.com>
2018-06-13 10:43:26 +1000

    Updated media.py
    - Updated comments 
    - PEP8 compliance

--- media.py
+++ media.py
@@ -1,6 +1,16 @@
 class Movie():
-    def __init__(self,movie_title,movie_storyline, poster_image,trailer_youtube):
-        self.title=movie_title
-        self.storyline=movie_storyline
-        self.poster_image_url=poster_image
-        self.trailer_youtube_url=trailer_youtube
+    """ Creates the movie abstarct class, which will be used to create movie instances.
+
+      Attributes:
+        movie_title: This is the title of the movie (String).
+        movie_storyline : This is the movie overview (String).
+        poster_image (str): This is path to the image (String).
+        trailer_youtube (str): This is path to the youtube movie link(String).
+    """
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
